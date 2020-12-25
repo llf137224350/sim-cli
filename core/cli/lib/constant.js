38e6aa9062bb56d1ee8617bd1a1c608ce6693c3e
llf137224350 <137224350@qq.com>
2020-12-25 22:38:57 +0800

    完成获取版本信息，node最低版本检查，root用户检查

---
+++ core/cli/lib/constant.js
@@ -0,0 +1,6 @@
+// node最低版本号
+const LOWEST_NODE_VERSION = '12.0.0';
+
+module.exports =  {
+  LOWEST_NODE_VERSION
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,7 +1,42 @@
 'use strict';
 
 module.exports = core;
+const semver = require('semver');
+const colors = require('colors/safe');
+const pkg = require('../package.json');
+const log = require('@sim-cli/log');
+const constant = require('./constant');
 
 function core() {
-  console.log('core');
+  try {
+    // 检查版本号
+    checkPkgVersion();
+    // 检查node版本号
+    checkNodeVersion();
+    // 检查是否为root权限使用sim-cli
+    checkRoot();
+    // 检查用户主目录
+
+  } catch (e) {
+    log.error('', e.message);
+  }
+}
+
+// 检查版本号
+function checkPkgVersion() {
+  log.info('', '当前版本：v' + pkg.version)
+}
+
+// 检查node版本号
+function checkNodeVersion() {
+  const currentVersion = process.version;
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`sim-cli 需要安装 v${lowestVersion} 以上版本的Node.js`));
+  }
+}
+
+// 检查是否为root权限使用sim-cli
+function checkRoot() {
+  require('root-check')();
 }
